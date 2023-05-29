3572a769152c21bd02e05a3999a12b1f1831ee20
aqdas-raja <aqdasraja66@gmail.com>
2023-05-28 22:09:40 -0700

    form with react+bootstrap

---
+++ src/Components/Signup.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Signup() {
+  return (
+    <div className='Signup template d-flex justify-content-center align-items-center vh-100 bg-primary'>
+        <div className=' form_container p-5  rounded bg-white'>
+        <form>
+            <h3 className='text-center'>Sign Up</h3>
+            <div className='mb-2'>
+                <label htmlFor="fname">First Name</label>
+                <input type='text' placeholder='Enter First Name' className='form-control'/>
+            </div>
+            <div className='mb-2'>
+                <label htmlFor="lname">Last Name</label>
+                <input type='text' placeholder='Enter Last Name' className='form-control'/>
+            </div>
+            <div className='mb-2'>
+                <label htmlFor="email">Email</label>
+                <input type='email' placeholder='Enter email' className='form-control'/>
+            </div>
+            <div className='mb-2'>
+                <label htmlFor="password">Password</label>
+                <input type='password' placeholder='Enter Password' className='form-control'/>
+            </div>
+                <div className='d-grid mt-2'>
+                 <button className='btn btn-primary'>Sign up</button>
+                </div>
+            
+            <p className='text-center mt-2'>
+                 Already Registered<Link to="/" className='ms-2'>Sign in</Link>
+            </p>
+        </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Signup;
